2a84ee5a195932ab395e0cb28a77d7d7a4b16033
Bhishm001 <63139521+Bhishm001@users.noreply.github.com>
2026-06-07 20:30:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function askAI(){
+ const q=document.getElementById('question').value;
+ document.getElementById('answer').innerHTML =
+ 'Connect this page to OpenAI or Gemini API. Question: '+q;
+}
