68ad32ae510cfe888b6326097a539e9e41a6e48f
Bhishm001 <63139521+Bhishm001@users.noreply.github.com>
2026-06-07 20:53:58 +0530

    script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function askAI() {
     const q = document.getElementById("question").value;
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=YOUR_API_KEY",
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AQ.Ab8RN6J5-M1m4VnflfPxRdaJig4UEMXUus1ffWz9qk-6OoDOJg",
       {
         method: "POST",
         headers: {
